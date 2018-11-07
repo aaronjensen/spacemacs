42629a6000f811c4ec1b5a2dd45234e363e7e4ec
deb0ch <thomas.de.beauchene@gmail.com>
2018-11-07 22:03:09 +0100

    shell: insert mode when entering vterm

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -218,7 +218,8 @@
       (setq shell-pop-window-position shell-default-position
             shell-pop-window-size     shell-default-height
             shell-pop-term-shell      shell-default-term-shell
-            shell-pop-full-span       shell-default-full-span)
+            shell-pop-full-span       shell-default-full-span
+            shell-pop-in-after-hook   (lambda () (call-interactively #'evil-insert)))
       (make-shell-pop-command "eshell" eshell)
       (make-shell-pop-command "term" term shell-pop-term-shell)
       (make-shell-pop-command "ansi-term" ansi-term shell-pop-term-shell)
